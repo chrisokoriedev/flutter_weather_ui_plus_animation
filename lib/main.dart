869b30e5ba22a39b51ef776d73f6345656ca4612
christian-iuil <okoriec01@gmail.com>
2023-10-22 16:07:48 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,16 +22,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DashBoard extends StatelessWidget {
+class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
 
   @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  VideoPlayerController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(
+      'assets/a.mp4',
+    )..initialize().then((_) {
+        _controller!.setLooping(true);
+        _controller!.play();
+        _controller!.setPlaybackSpeed(0.4);
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller!.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-        ],
-      )
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Stack(
+            children: [
+              _controller!.value.isInitialized
+                  ? SizedBox(
+                      width: constraints.maxWidth,
+                      height: constraints.maxHeight,
+                      child: VideoPlayer(_controller!),
+                    )
+                  : Container()
+            ],
+          );
+        },
+      ),
     );
   }
 }
